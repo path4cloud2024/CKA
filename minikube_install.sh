261a9731c8fa6c6fbfb5b2a3b360d25f9937accc
Amit <amitaryan0010@gmail.com>
2026-03-28 00:52:57 +0530

    updated notes

---
+++ minikube_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#######################################
+lsb_release -a
+echo '### installing Docker....'
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+#######################################
+echo '### Installing Kubectl...'
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+# If above source url doesn't work then comment above 3 lines and uncomment below 4 lines
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+# echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+echo '### Installing minikube...'
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#######################################
+echo '### Adding user to docker group...'
+sudo usermod -aG docker $USER
+newgrp docker
+
+#######################################
+echo "minikube server is ready, to start the cluster, run minikube start"
+
+echo "For more help check the minikube_install.txt file"
